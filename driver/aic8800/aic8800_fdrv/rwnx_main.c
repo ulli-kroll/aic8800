c909b4c3d1f01ddcbdee9b85e857988537638ed8
Hans Ulli Kroll <linux@ulli-kroll.de>
2025-10-22 11:40:54 +0200

    fix-linux-6.14-build.patch
    Signed-off-by: Hans Ulli Kroll <linux@ulli-kroll.de>

--- driver/aic8800/aic8800_fdrv/rwnx_main.c
+++ driver/aic8800/aic8800_fdrv/rwnx_main.c
@@ -3577,6 +3577,9 @@ static int rwnx_cfg80211_set_tx_power(struct wiphy *wiphy, struct wireless_dev *
 static int rwnx_cfg80211_get_tx_power(struct wiphy *wiphy,
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
  struct wireless_dev *wdev,
+#if LINUX_VERSION_CODE >= KERNEL_VERSION (6, 14, 0)
+ unsigned int link_id,
+#endif
 #endif
 	int *mbm)
 {
