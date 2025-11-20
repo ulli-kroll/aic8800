8e9358170ced75304aea8375a9ea65e076535dd3
Hans Ulli Kroll <linux@ulli-kroll.de>
2025-11-20 13:22:53 +0100

    resolve HIGH_KERNEL_VERSION
    Signed-off-by: Hans Ulli Kroll <linux@ulli-kroll.de>

--- aicsemi/aic8800/aic8800_fdrv/rwnx_msg_rx.c
+++ aicsemi/aic8800/aic8800_fdrv/rwnx_msg_rx.c
@@ -1003,8 +1003,8 @@ static inline int rwnx_rx_sm_connect_ind(struct rwnx_hw *rwnx_hw,
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
             struct cfg80211_roam_info info;
             memset(&info, 0, sizeof(info));
-            
-#if LINUX_VERSION_CODE < HIGH_KERNEL_VERSION
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 0, 0)
 			if (rwnx_vif->ch_index < NX_CHAN_CTXT_CNT)
     			info.channel = rwnx_hw->chanctx_table[rwnx_vif->ch_index].chan_def.chan;
 			info.bssid = (const u8 *)ind->bssid.array;
@@ -1013,7 +1013,7 @@ static inline int rwnx_rx_sm_connect_ind(struct rwnx_hw *rwnx_hw,
 			if (rwnx_vif->ch_index < NX_CHAN_CTXT_CNT)
     			info.links[0].channel = rwnx_hw->chanctx_table[rwnx_vif->ch_index].chan_def.chan;
 			info.links[0].bssid = (const u8 *)ind->bssid.array;;
-#endif//LINUX_VERSION_CODE < HIGH_KERNEL_VERSION    
+#endif//LINUX_VERSION_CODE < KERNEL_VERSION(6, 0, 0)
 
             info.req_ie = req_ie;
             info.req_ie_len = ind->assoc_req_ie_len;
