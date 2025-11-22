8b358cfea9b8a8f8841b93563558fa86236a052e
Hans Ulli Kroll <linux@ulli-kroll.de>
2025-11-22 21:20:33 +0100

    remove KERNEL_VERSION(5, 6, 0)
    Signed-off-by: Hans Ulli Kroll <linux@ulli-kroll.de>

--- aicsemi/aic8800/aic8800_fdrv/rwnx_msg_rx.c
+++ aicsemi/aic8800/aic8800_fdrv/rwnx_msg_rx.c
@@ -654,17 +654,10 @@ static inline int rwnx_rx_scanu_result_ind(struct rwnx_hw *rwnx_hw,
     chan = ieee80211_get_channel(rwnx_hw->wiphy, ind->center_freq);
 
     if (chan != NULL) {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 6, 0)
-        struct timespec ts;
-        ts = ktime_to_timespec(ktime_get_boottime());
-        tsf = (u64)ts.tv_sec * 1000000 + div_u64(ts.tv_nsec, 1000);
-        mgmt->u.probe_resp.timestamp = tsf;
-#else
         struct timespec64 ts;
         ts = ktime_to_timespec64(ktime_get_boottime());
         tsf = (u64)ts.tv_sec * 1000000 + div_u64(ts.tv_nsec, 1000);
         mgmt->u.probe_resp.timestamp = tsf;
-#endif
         ie = mgmt->u.probe_resp.variable;
         ielen = len - offsetof(struct ieee80211_mgmt, u.probe_resp.variable);
         beacon_interval = le16_to_cpu(mgmt->u.probe_resp.beacon_int);
