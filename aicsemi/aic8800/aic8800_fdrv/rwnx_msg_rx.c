aaadec657cb4235f42f8d52a0368be1773f216f9
Hans Ulli Kroll <linux@ulli-kroll.de>
2025-11-22 20:07:23 +0100

    remove KERNEL_VERSION(4, 8, 0)
    Signed-off-by: Hans Ulli Kroll <linux@ulli-kroll.de>

--- aicsemi/aic8800/aic8800_fdrv/rwnx_msg_rx.c
+++ aicsemi/aic8800/aic8800_fdrv/rwnx_msg_rx.c
@@ -569,20 +569,14 @@ static inline int rwnx_rx_scan_done_ind(struct rwnx_hw *rwnx_hw,
                                         struct rwnx_cmd *cmd,
                                         struct ipc_e2a_msg *msg)
 {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
     struct cfg80211_scan_info info = {
         .aborted = false,
     };
-#endif
     RWNX_DBG(RWNX_FN_ENTRY_STR);
 
     rwnx_ipc_elem_var_deallocs(rwnx_hw, &rwnx_hw->scan_ie);
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
     ieee80211_scan_completed(rwnx_hw->hw, &info);
-#else
-    ieee80211_scan_completed(rwnx_hw->hw, false);
-#endif
 
     return 0;
 }
@@ -596,11 +590,9 @@ static inline int rwnx_rx_scanu_start_cfm(struct rwnx_hw *rwnx_hw,
                                           struct ipc_e2a_msg *msg)
 {
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
 			struct cfg80211_scan_info info = {
 				.aborted = false,
 			};
-#endif
 
     RWNX_DBG(RWNX_FN_ENTRY_STR);
 
@@ -611,11 +603,7 @@ static inline int rwnx_rx_scanu_start_cfm(struct rwnx_hw *rwnx_hw,
 		){
 
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)
         cfg80211_scan_done(rwnx_hw->scan_request, &info);
-#else
-        cfg80211_scan_done(rwnx_hw->scan_request, false);
-#endif
     } 
 
 
